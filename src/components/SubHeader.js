b9054bc0ceee6ca257f21d242d201b5d682b332a
sailingdev <sailingdev123@gmail.com>
2021-01-07 03:26:57 +0800

    Styling subheader content

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledSubHeader = styled.div`
+  	height: 40px;
+  	display: flex;
+  	align-items: center;
+  	background-color: #232f3e;
+  	position: sticky;
+  	top: 0;
+    padding: 0 20px;
+    color: white;
+    z-index: 2;
+`;
 
 class SubHeader extends Component {
 
 	render() {
 		return (
-			<div>
+			<StyledSubHeader>
 				SubHeader
-			</div>
+			</StyledSubHeader>
 
 		);
 	}
