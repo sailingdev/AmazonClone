fe7dc2b2f7931d7da478227bf08ef6ec19bcede8
sailingdev <sailingdev123@gmail.com>
2021-01-07 03:48:49 +0800

    Add styled image in prime dropdown

--- src/utils/PrimeDropDown.js
+++ src/utils/PrimeDropDown.js
@@ -5,12 +5,26 @@ const StyledDropDownContent = styled.div`
     background: #00a8e1;
 `;
 
+const StyledImg = styled.div`
+    width: 33%;
+    height: auto;
+    margin: 0 auto;
+    padding: 20px 0px 15px 0px;
+    max-width: 100%;
+
+    img {
+        max-width: 100%;
+    }
+`;
+
 class PrimeDropDown extends Component {
 
 	render() {
 		return (
 			<StyledDropDownContent>
-				PrimeDropDown
+                <StyledImg>
+                    <img src="https://m.media-amazon.com/images/G/01/marketing/prime/JoyDelivered/prime_logo_RGB_PRIME_OAT._CB439327201_.png" alt="" />
+                </StyledImg>
 			</StyledDropDownContent>
 
 		);
