9cff692a06c261ade10ae13cfdfb8e081fb92d8d
sailingdev <sailingdev123@gmail.com>
2021-01-07 02:09:47 +0800

    Configure Route to Home page in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home.js';
 
 function App() {
   return (
     <div className="App">
-      Amazon
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
