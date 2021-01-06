9e735ad06d3453395d5c1cc903ffae95d439c442
sailingdev <sailingdev123@gmail.com>
2021-01-07 04:59:24 +0800

    Styled products component content

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledProducts = styled.div`
+    padding: 0 10px 0;
+`;
+
+const StyledProductsContent = styled.div`
+    max-width: 1480px;
+    margin: 0 auto;
+    overflow: hidden;
+    padding: 20px 0;
+`;
 
 class Products extends Component {
 
 	render() {
 		return (
-			<div>
-				Products
-			</div>
+			<StyledProducts>
+				<StyledProductsContent>
+                    
+                </StyledProductsContent>
+			</StyledProducts>
 
 		);
 	}
