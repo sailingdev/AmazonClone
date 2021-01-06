f077fbffdf4c1a12cbf17014e9e52ac7d52ad4aa
sailingdev <sailingdev123@gmail.com>
2021-01-07 04:11:40 +0800

    Make empty sidebar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+
+	render() {
+		return (
+			<div>
+				SideBar
+			</div>
+
+		);
+	}
+}
+
+export default SideBar;
