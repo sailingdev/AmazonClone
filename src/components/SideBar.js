2eacc5b23c0223301b068db71605dd9dcff9651f
sailingdev <sailingdev123@gmail.com>
2021-01-07 04:16:34 +0800

    Add side bar background

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledSideBar = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 100000;
+    color: black;
+    background-color: rgba(0,0,0, .8);
+    animation: fadeIn 0.5s;
+`;
 
 class SideBar extends Component {
 
 	render() {
 		return (
 			<div>
-				SideBar
+				<StyledSideBar></StyledSideBar>
 			</div>
 
 		);
