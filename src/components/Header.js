2848d93c8d3fbff77bbc60cd5ffb3476e533366c
sailingdev <sailingdev123@gmail.com>
2021-01-07 02:22:19 +0800

    Styling Header content & Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+  	height: 60px;
+  	display: flex;
+  	align-items: center;
+  	background-color: #131921;
+	@media (max-width: 768px) {
+		justify-content: space-between;
+	}
+`;
+
+const StyledLogo = styled.img`
+	width: 100px;
+  	object-fit: contain;
+  	margin: 0 20px;
+  	margin-top: 18px;
+`;
 
 class Header extends Component {
 
 	render() {
 		return (
 			<div>
-				Header
+				<StyledHeader>
+                    <a href="/">
+						<StyledLogo 
+							src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+						/>
+					</a>
+                </StyledHeader>
 			</div>
 
 		);
