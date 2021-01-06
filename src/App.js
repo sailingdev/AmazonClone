e0c5d580dc3e1933dfbd74b708f57278c95dd4d6
sailingdev <sailingdev123@gmail.com>
2021-01-07 02:14:53 +0800

    Adding Header component, and also in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+	render() {
+		return (
+			<div>
+				Header
+			</div>
+
+		);
+	}
+}
+
+export default Header;
