47064b3603f794ef2be218815627e22ab3444a8d
sailingdev <sailingdev123@gmail.com>
2021-01-07 03:10:40 +0800

    Add empty accountlist dropdown component

---
+++ src/utils/AccountListDropDown.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AccountListDropDown extends Component {
+
+	render() {
+		return (
+			<div>
+				AccountListDropDown
+			</div>
+
+		);
+	}
+}
+
+export default AccountListDropDown;
