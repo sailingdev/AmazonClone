c19cddbe1e190adcba7e1f6cb7083be0e7392d04
sailingdev <sailingdev123@gmail.com>
2021-01-07 03:46:52 +0800

    Styling empty prime dropdown content

--- src/utils/PrimeDropDown.js
+++ src/utils/PrimeDropDown.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledDropDownContent = styled.div`
+    background: #00a8e1;
+`;
 
 class PrimeDropDown extends Component {
 
 	render() {
 		return (
-			<div>
+			<StyledDropDownContent>
 				PrimeDropDown
-			</div>
+			</StyledDropDownContent>
 
 		);
 	}
