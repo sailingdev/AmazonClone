a9dc7323ab8a08c381a7cee476eddd008af89208
sailingdev <sailingdev123@gmail.com>
2021-01-07 02:35:33 +0800

    Implement location part in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,6 +19,67 @@ const StyledLogo = styled.img`
   	margin-top: 18px;
 `;
 
+const StyledLocation = styled.div`
+	display: flex;
+	align-items: flex-end;
+	color: white;
+	cursor: pointer;
+	padding: 4px 8px 4px 6px;
+
+	&:hover {
+		cursor: pointer;
+		border: 1px solid;
+		border-radius: 2px;
+		outline: 0;
+		padding: 3px 7px 3px 5px;
+
+		div {
+			border: none;
+			padding: none;
+		}
+	}
+	@media (max-width: 768px) {
+		display: none;
+	}
+`;
+
+const StyledOption = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-left: 10px;
+	margin-right: 10px;
+	color: white;
+	position: relative;
+	padding: 4px 8px 4px 6px;
+
+	&:hover {
+		cursor: pointer;
+		border: 1px solid;
+		border-radius: 2px;
+		outline: 0;
+		padding: 3px 7px 3px 5px;
+	}
+
+	@media (max-width: 768px) {
+		display: none;
+	}
+`;
+
+const StyledOptionLineOne = styled.span`
+	font-size: 10px;
+`;
+
+const StyledOptionLineTwo = styled.span`
+	text-align: center;
+	font-size: 13px;
+	font-weight: 800;
+	display: flex;
+	align-items: center;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
 class Header extends Component {
 
 	render() {
@@ -30,6 +91,16 @@ class Header extends Component {
 							src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
 						/>
 					</a>
+                    <StyledLocation>
+						<LocationOnIcon fontSize="small" />
+						<StyledOption>
+							<StyledOptionLineOne>Hello</StyledOptionLineOne>
+							<StyledOptionLineTwo>
+								Select your address
+							</StyledOptionLineTwo>
+
+						</StyledOption>
+					</StyledLocation>
                 </StyledHeader>
 			</div>
 
