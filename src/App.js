146e1e3bd259616ccf5b2d5d050382cb82d43b9e
sailingdev <sailingdev123@gmail.com>
2021-01-09 02:41:22 +0800

    Add Footer in home page  and styling Footer content

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home.js';
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Switch>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
       </Router>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledItemLine = styled.div`
+	padding: 25px;
+`;
+
+const StyledFooter = styled.div`
+	display: flex;
+	flex-direction: column;
+	background-color: #232F3E;
+	text-align: center;
+	margin: auto;
+	color: white;
+
+	${StyledItemLine}:first-child {
+		border-bottom: 1px solid #3a4553;
+	}
+
+	${StyledItemLine}:nth-child(n+3) {
+		background: #131A22;
+	}
+`;
+
 
 class Footer extends Component {
 
 	render() {
 		return (
-			<div>
-				Footer
-			</div>
-
+			<StyledFooter>
+				<StyledItemLine>Accessiblity</StyledItemLine>
+				<StyledItemLine>Item</StyledItemLine>
+				<StyledItemLine>Description</StyledItemLine>
+				<StyledItemLine>Copyright</StyledItemLine>
+			</StyledFooter>
 		);
 	}
 }
